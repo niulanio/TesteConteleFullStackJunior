77a18dfee89517de8abc57321e517bb20cb88c0e
Niulânio Bezerra <niulanio.xd@gmail.com>
2022-05-02 21:09:48 -0300

    Finish configure docker

--- knexfile.js
+++ knexfile.js
@@ -18,14 +18,14 @@ module.exports = {
       database: process.env.POSTGRES_DB || "TestDeveloperFullStackContele",
       user: process.env.POSTGRES_USER || "postgres",
       password: process.env.POSTGRES_PASSWORD || "123",
-      port: 5433
+      port: 5432
     },
     pool: {
       min: 2,
       max: 10,
     },
     migrations: {
-      directory: './migrations'
+      tableName: 'knex_migrations'
     }
   },
 };
